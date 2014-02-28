14f09804239834441f5f7113ca51c5341b96dccb
Doug Emery <emeryr@upenn.edu>
2014-02-28 10:48:57 -0500

    clean up code

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,5 @@
 jQuery(function() {
-
     
-  Kalendar.start("#kalendar")
-
-    // $('#create-ms').submit(function(e){
-    //     e.preventDefault();
-    //     return $(this).serializeArray();
-    // });
+  Kalendar.start("#kalendar");
 
 })
