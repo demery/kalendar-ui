fdff5c249f416b84d192d0959efbf60f8639ed8c
Doug Emery <emeryr@upenn.edu>
2014-08-13 18:05:09 -0400

    Collect next folio information

--- public/js/kalendarium-ui.js
+++ public/js/kalendarium-ui.js
@@ -54,8 +54,6 @@ $(document).ready(function(){
           var next = (side == 2) ? ([num + 1, 1]) : ([num, 2]);
           folios.push(next);
         };
-        // create holder for calendar properties
-        $.kui.calendar = {};
         $.kui.calendar.folios = folios;
       },
       error: function(data) {
@@ -86,7 +84,7 @@ $(document).ready(function(){
         "viewingHint":"paged",
       }],
     };
-    var rv = [ null, 'r', 'v' ]
+    var rv = [ null, 'r', 'v' ];
     manifest.sequences[0].canvases = _.map($.kui.calendar.folios, function(fol) {
       var folNum = String(fol[0]) + rv[fol[1]];
       return {
@@ -114,14 +112,143 @@ $(document).ready(function(){
     });
   };
 
+  window.kuiNextFolio = function() {
+    // get current folio index
+    var folioIndex = $.kui.calendar.currFolio['index'];
+    // get the next index
+    folioIndex = folioIndex === null ? 0 : folioIndex + 1;
+    if (folioIndex < $.kui.calendar.folios.length) {
+      kuiNextFolioForm(folioIndex);
+    } else {
+      // @todo: last folio behavior ? return to 0?
+    };
+  };
+
+  window.kuiNextFolioForm = function(folioIndex) {
+    // create a human friendly list of folios
+    var rv = [ null, 'r', 'v' ];
+    var folioNums = _.map($.kui.calendar.folios, function(fol) {
+      return String(fol[0]) + rv[fol[1]];
+    });
+
+    // create the form elements
+    elements = $.kui.calendar.nextFolioElements;
+    _.each(elements, function(ele) {
+      // $this = $(this);
+      var $formElement = $('<div class="form-group"></div>');
+
+      if (ele.label) {
+        var $elementLabel = $('<label class="control-label" for="#nextFol-val-' + ele.element + '">' + ele.label + '</label>');
+      };
+
+      if (ele.fieldtype === 'text') {
+        var $elementContainer = $('<div class=""><input type="textfield" class="form-control" id="nextFol-val-' + ele.element + '"></div>');
+
+      // Add a hidden field input object
+      } else if (ele.fieldtype === 'hidden') {
+        var $elementContainer = $('<div class=""><input type="hidden" class="form-control" id="nextFol-val-' + ele.element + '"></div>');
+
+      // Add a select field object
+      } else if (ele.fieldtype === 'list') {
+        var $elementContainer = $('<div class=""><select class="form-control" id="nextFol-val-' + ele.element + '"></select>');
+
+        var elementOptions = '';
+
+        _.each(ele.options, function(val, key) {
+          elementOptions +='<option value= ' + key + '>' + val + '</option>'
+        })
+
+        $elementContainer.find('select').append(elementOptions)
+
+      // Groups handle more complex types, including dates, grading, and dimensions
+      };
+
+      $formElement.append($elementLabel, $elementContainer);
+      $('#kui-fields').append($formElement);
+    });
+
+    var folioOptions = '';
+    _.each(folioNums, function(fol, index) {
+      folioOptions += '<option value="' + index + '">' + fol + '</option>';
+    });
+
+    // select the correct folio
+    $('#nextFol-val-index').append(folioOptions);
+    $('#nextFol-val-index').find('option[value="' + folioIndex + '"]').attr('selected', 'selected');
+
+    // add a button
+    $('#kui-fields').append('<input type="button" id="nextFol-edit-btn" class="btn btn-default" value="Edit"/>')
+    // set the button value to the selected folio
+    $('#nextFol-val-index').find('option[value="' + folioIndex + '"]').attr('selected', 'selected');
+    $('#nextFol-edit-btn').val('Edit ' + $('#nextFol-val-index option[selected]').text());
+    // update the edit button to the currently selected folio
+    $('#nextFol-val-index').change(function() {
+      // console.log(this);
+      $('#nextFol-edit-btn').val('Edit ' + $(this).find('option:selected').text());
+    });
+
+    $('#nextFol-edit-btn').on('click', function() {
+      kuiUpdateCurrFolio();
+    });
+
+    $('#kalendar').show();
+  };
+
+  window.kuiEditFolioForm = function() {
+
+    $.ajax()
+  };
+
+  window.kuiUpdateCurrFolio = function() {
+    _.each($('#kui-fields input, #kui-fields select'), function(ele) {
+      var name = $(ele).attr('id').substr(12);
+      var val = $(ele).val();
+      _.each($.kui.calendar.currFolio, function(v,k) {
+        if (k === name) {$.kui.calendar.currFolio[k] = val; }
+      });
+    });
+  };
+
   window.kuiStartKalendar = function(ms_id) {
     var lookup = kuiLookUpManuscript(ms_id);
     var mf = lookup.then(kuiCreateManifest);
+
+    // Set up the form
+    $form = $('<form id="kui" role="form"><div id="kui-messages" class="alert"></div><div id="kui-fields"></div></form>');
+    $('#kalendar').append($form);
+
     mf.done(function(data) {
       var id = data['@id'];
       _.findWhere($.kmw, {'element': 'sc_cal_manifest_id'}).v = id;
+      // make sure we update $.kmw and submit to the manuscript service
       kmwFormUpdate();
       kmwSubmitEdit(ms_id);
+      kuiNextFolio();
     });
   };
+
+  $.kui = {
+    'manuscript': {},
+    'calendar': {
+      'folios': [],
+      'currFolio': {
+        'index': null,
+        'month': null,
+        'startDay': null,
+        'numOfDays': null,
+      },
+      'nextFolioElements': [
+        { 'element':'index', 'label':'Folio', 'v':'', 'fieldtype':'list', 'options':{} },
+        { 'element':'month', 'label':'Month', 'v':'', 'fieldtype':'list', 'options':{
+          '0':'', '1':'Jan', '2':'Feb', '3':'Mar', '4':'Apr', '5':'May', '6':'Jun', '7':'Jul', '8':'Aug', '9':'Sep', '10':'Oct', '11':'Nov', '12':'Dec'
+        }},
+        { 'element':'startDay', 'label':'Starting on', 'v':'', 'fieldtype':'list', 'options':{
+          '0':'', '1':'1', '2':'2', '3':'3', '4':'4', '5':'5', '6':'6', '7':'7', '8':'8', '9':'9', '10':'10', '11':'11', '12':'12', '13':'13', '14':'14', '15':'15', '16':'16', '17':'17', '18':'18', '19':'19', '20':'20', '21':'21', '22':'22', '23':'23', '24':'24', '25':'25', '26':'26', '27':'27', '28':'28', '29':'29', '30':'30', '31':'31'
+        }},
+        { 'element':'numOfDays', 'label':'Number of days', 'v':'', 'fieldtype':'text', 'options':{} },
+
+      ],
+    },
+  }
+
 });
